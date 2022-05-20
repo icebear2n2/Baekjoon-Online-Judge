c1a71cab8f035f77dc39606c812d9a2b587d6cf7
icebear2n2 <icebear2n2@gmail.com>
2022-05-20 22:06:32 +0900

    ing..

--- 4673.py
+++ 4673.py
@@ -1,3 +1,16 @@
-import sys
-
-?
+m = list(range(1, 101))
+n = 1
+num = 1
+while True:
+    if n < 10:
+            n+=n
+            print(n)
+            m.remove(n)
+    else:
+        n = n + (n // 10) + (n % 10)
+        print(n)
+        if n < 100:
+            m.remove(n)
+        else:
+            break
+print(m)
