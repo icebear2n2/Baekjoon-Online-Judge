86d5d99841ff6fd008cdc9f4a660581ba001afe3
icebear2n2 <icebear2n2@gmail.com>
2022-05-21 18:11:20 +0900

    mistake

--- 4673.py
+++ 4673.py
@@ -1,16 +1,1 @@
-m = list(range(1, 101))
-n = 1
-num = 1
-while True:
-    if n < 10:
-            n+=n
-            print(n)
-            m.remove(n)
-    else:
-        n = n + (n // 10) + (n % 10)
-        print(n)
-        if n < 100:
-            m.remove(n)
-        else:
-            break
-print(m)
+# 첨부터 while이다. 와.. 왜 for문 쓰고 있었찌
