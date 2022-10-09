6d7b00067c8f65ed882c12858222784603e25975
icebear2n2 <icebear2n2@gmail.com>
2022-10-09 13:57:26 +0900

    2675

--- 2675.py
+++ 2675.py
@@ -1,2 +1,14 @@
-alpha='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ\$%*+-./:'
-alpha=list(alpha)
+n = int(input())
+
+for i in range(n):
+    result=[]
+    num, char = map(str, input().split())
+    char = list(char)
+    for ch in range(len(char)):
+        
+        for j in range(int(num)):
+            result.append(char[ch])
+
+    result = ''.join(result)
+    print(result)
+        
