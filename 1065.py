50512ec5043f5783a13149f3aa613cc8223b5553
icebear2n2 <icebear2n2@gmail.com>
2022-10-16 15:11:37 +0900

    1065

--- 1065.py
+++ 1065.py
@@ -1,4 +1,12 @@
-import sys
+n = int(input())
+count = 0
 
-n = int(sys.stdin.readline())
+if n >= 100:
+    for i in range(100, n+1):
+        i_str = str(i)
+        if (int(i_str[1]) - int(i_str[0])) == (int(i_str[2]) - int(i_str[1])):
+            count += 1
+    print(count+99)
+else:
+    print(n)
 
