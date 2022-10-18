ad0260ebc316ed22f0b0bce029256cecb360e03b
icebear2n2 <icebear2n2@gmail.com>
2022-10-18 18:12:37 +0900

    1193 %80

--- 1193.py
+++ 1193.py
@@ -1,1 +1,43 @@
-모르것다!!
+lst2 = [1, 1]
+count = 2
+
+for k in range(2, 6):
+
+    lst = [ 0 for i in range(k) ]
+    
+    if k % 2 == 0:
+        for i in range(len(lst)+1):
+            if i == 0:
+                lst2[0] = 1
+                lst2[1] = len(lst)
+                print(count)
+                print(lst2)
+                count+=1
+            elif i > 1 and len(lst) in lst2:
+                num = lst2
+                break
+            else:
+                lst2[0] += 1
+                lst2[1] -= 1
+                print(count)
+                print(lst2)
+                count+=1
+    else:
+        for i in range(len(lst)+1):
+            if i == 0:
+                lst2[0] = len(lst)
+                lst2[1] = 1
+                print(count)
+                print(lst2)
+                count+=1
+                
+                
+            elif i > 1 and len(lst) in lst2:
+                num = lst2
+                break
+            else:
+                lst2[0] -= 1
+                lst2[1] += 1
+                print(count)
+                print(lst2)
+                count+=1
