b4048f4f2df64eef450d2c98b81378f49f457d53
icebear2n2 <icebear2n2@gmail.com>
2022-10-19 00:10:35 +0900

    1193 90%

--- 1193.py
+++ 1193.py
@@ -1,43 +1,42 @@
 lst2 = [1, 1]
-count = 2
-
-for k in range(2, 6):
-
+count = 1
+x = int(input())
+k = 2
+while True:
+    if count == x:
+        break
     lst = [ 0 for i in range(k) ]
     
     if k % 2 == 0:
         for i in range(len(lst)+1):
+            if count == x:
+                break
             if i == 0:
+                count += 1
                 lst2[0] = 1
                 lst2[1] = len(lst)
-                print(count)
-                print(lst2)
-                count+=1
             elif i > 1 and len(lst) in lst2:
                 num = lst2
                 break
             else:
+                count += 1
                 lst2[0] += 1
                 lst2[1] -= 1
-                print(count)
-                print(lst2)
-                count+=1
     else:
         for i in range(len(lst)+1):
+            if count == x:
+                break
             if i == 0:
+                count += 1
                 lst2[0] = len(lst)
                 lst2[1] = 1
-                print(count)
-                print(lst2)
-                count+=1
-                
-                
             elif i > 1 and len(lst) in lst2:
                 num = lst2
                 break
             else:
+                count += 1
                 lst2[0] -= 1
                 lst2[1] += 1
-                print(count)
-                print(lst2)
-                count+=1
+    k+=1
+
+print("{}/{}".format(lst2[0], lst2[1]))
