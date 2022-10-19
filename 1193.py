b26e8f448a31faff3cb7e2f610f8eec4d642e9ed
icebear2n2 <icebear2n2@gmail.com>
2022-10-19 15:56:46 +0900

    모르것다

--- 1193.py
+++ 1193.py
@@ -1,12 +1,28 @@
-def odd(count):
-    # [len, 1]
-    # [-1, +1]
-    pass
+count = 1
 
-def even(count):
-    # [1, len]
-    # [-1, +1]
-    pass
-
-
-
+x = int(input())
+i = 1
+lst = [1,1]
+while True:
+    i+=1
+    if count == x:
+        break
+    for j in range(i):
+        if count == x:
+            break
+        count += 1
+        if i % 2 == 0:
+            if j == 0:
+                lst[0] = 1
+                lst[1] = i
+            else:
+                lst[0] += 1
+                lst[1] -= 1
+        else:
+            if j == 0:
+                lst[0] = i
+                lst[1] = 1
+            else:
+                lst[0] -= 1
+                lst[1] += 1
+print("{}/{}".format(lst[0], lst[1]))
