1188ac920b559123cd431c9bc1784f0f41d42b84
icebear2n2 <icebear2n2@gmail.com>
2022-10-19 13:53:56 +0900

    1193 %10

--- 1193.py
+++ 1193.py
@@ -1,35 +1,12 @@
-import sys
-lst = [1, 1]
-count = 1
+def odd(count):
+    # [len, 1]
+    # [-1, +1]
+    pass
 
-x = int(sys.stdin.readline())
-i = 1
-while True:
-    i += 1
-    if count == x:
-        break
-    for j in range(i):
-        if count == x:
-            break
-        if i % 2 == 0:
-            count += 1
-            if j == 0:
-                lst[0] = 1
-                lst[1] = i
-            elif j > 1 and i in lst:
-                break
-            else:
-                    lst[0] += 1
-                    lst[1] -= 1
-            
-        else:
-            count += 1
-            if j == 0:
-                lst[0] = i
-                lst[1] = 1
-            elif j > 1 and i in lst:
-                break
-            else:
-                    lst[0] -= 1
-                    lst[1] += 1
-print("{}/{}".format(lst[0], lst[1]))
+def even(count):
+    # [1, len]
+    # [-1, +1]
+    pass
+
+
+
