840d620eb9524d575f768f0ee13294113dbf7085
icebear2n2 <icebear2n2@gmail.com>
2022-10-19 11:01:37 +0900

    1193 91%

--- 1193.py
+++ 1193.py
@@ -1,42 +1,35 @@
-lst2 = [1, 1]
+import sys
+lst = [1, 1]
 count = 1
-x = int(input())
-k = 2
+
+x = int(sys.stdin.readline())
+i = 1
 while True:
+    i += 1
     if count == x:
         break
-    lst = [ 0 for i in range(k) ]
-    
-    if k % 2 == 0:
-        for i in range(len(lst)+1):
-            if count == x:
-                break
-            if i == 0:
-                count += 1
-                lst2[0] = 1
-                lst2[1] = len(lst)
-            elif i > 1 and len(lst) in lst2:
-                num = lst2
+    for j in range(i):
+        if count == x:
+            break
+        if i % 2 == 0:
+            count += 1
+            if j == 0:
+                lst[0] = 1
+                lst[1] = i
+            elif j > 1 and i in lst:
                 break
             else:
-                count += 1
-                lst2[0] += 1
-                lst2[1] -= 1
-    else:
-        for i in range(len(lst)+1):
-            if count == x:
-                break
-            if i == 0:
-                count += 1
-                lst2[0] = len(lst)
-                lst2[1] = 1
-            elif i > 1 and len(lst) in lst2:
-                num = lst2
+                    lst[0] += 1
+                    lst[1] -= 1
+            
+        else:
+            count += 1
+            if j == 0:
+                lst[0] = i
+                lst[1] = 1
+            elif j > 1 and i in lst:
                 break
             else:
-                count += 1
-                lst2[0] -= 1
-                lst2[1] += 1
-    k+=1
-
-print("{}/{}".format(lst2[0], lst2[1]))
+                    lst[0] -= 1
+                    lst[1] += 1
+print("{}/{}".format(lst[0], lst[1]))
