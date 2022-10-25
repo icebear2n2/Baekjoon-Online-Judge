4135b6fcfeb24bab16023a1ae4a6deaccfaf21e8
icebear2n2 <icebear2n2@gmail.com>
2022-10-25 17:02:46 +0900

    2738 success

--- 2738.py
+++ 2738.py
@@ -1,3 +1,9 @@
-모르것다!!!!!!
+n, m = map(int, input().split())
+lst = []
 
-        
+for i in range(n+n):
+    lst.append(list(map(int, input().split())))
+
+for i in range(n):
+    lst2 = [lst[i][j] + lst[i+n][j] for j in range(m)]
+    print(" ".join(map(str, lst2)))
