dde593070a93e711d22e3cd9ef93efde88398d9b
icebear2n2 <icebear2n2@gmail.com>
2022-10-25 15:42:16 +0900

    failed

--- 2738.py
+++ 2738.py
@@ -1,8 +1,3 @@
-n, m = map(int, input().split())
-lst = []
-lst2 = []
-for i in range(n+m):
-    if i < n:
-        lst += list(map(int, input().split()))
-    else:
-        lst2 += list(map(int, input().split()))
+모르것다!!!!!!
+
+        
