9a9d9632b97fb7ad8e8eab785c5763a372375dab
icebear2n2 <icebear2n2@gmail.com>
2022-10-25 12:01:22 +0900

    2738

--- 2738.py
+++ 2738.py
@@ -1,7 +1,14 @@
 n, m = map(int, input().split())
 lst = []
+lst2 = []
 for i in range(n+m):
-    lst += list(map(int, input().split()))
-    
+    if i < n:
+        lst += [list(map(int, input().split()))]
+    else:
+        lst2 += [list(map(int, input().split()))]
 
-print(lst)
+for i in range(len(lst)):
+    c = [lst[i][j] + lst2[i][j] for j in range(len(lst))]
+    for i in c:
+        print(i, end=' ')
+    print()
