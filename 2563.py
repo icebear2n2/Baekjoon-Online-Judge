9fd87ad0fe0f4d799e82b10836e5676f67b4aaf5
icebear2n2 <icebear2n2@gmail.com>
2022-10-28 13:57:08 +0900

    2563 80%

--- 2563.py
+++ 2563.py
@@ -1,9 +1,22 @@
 n = int(input())
 lst = []
+lst2 = []
+lst3 = []
+
 for i in range(n):
     lst += [list(map(int, input().split()))]
 
-num1 = 10 - (lst[2][0] - lst[0][0])
-num2 = (lst[0][1] + 10) - (lst[0][1] - lst[2][1]) - lst[0][1]
+for i in range(len(lst)):
+    lst2.append(lst[i][0])
+    lst3.append(lst[i][1]) 
 
-print((100*n) - (num1*num2))
+lst2.sort()
+lst3.sort()
+
+num = (lst2[-1] - lst2[1]) - (lst2[1] - lst2[0])
+
+num2 = (lst3[-1] - lst3[0])
+
+result = num * num2
+
+print((100*n) - (result))
