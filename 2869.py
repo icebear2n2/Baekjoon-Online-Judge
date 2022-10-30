14ed6dbe500bf31c472841caa05e75af95953f87
icebear2n2 <icebear2n2@gmail.com>
2022-10-30 20:46:23 +0900

    2869 failed

--- 2869.py
+++ 2869.py
@@ -1,4 +1,15 @@
 A, B, V = map(int, input().split())
 
+Day = 0
+num = 0
 
+if (A - B) == 1:
+    print(V - B)
+else:
+    while num < V:
+        Day += 1
+        num += (A - B)
+
+    print(Day)
+        
         
