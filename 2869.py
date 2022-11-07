5963f98b42407e9c793f867af708ef6da4b8a1d3
icebear2n2 <icebear2n2@gmail.com>
2022-11-07 14:07:28 +0900

    2869

--- 2869.py
+++ 2869.py
@@ -1,9 +1,8 @@
+import math
+
 A, B, V = map(int, input().split())
 
 
-if round(V / A) * A > V:
-    print(round(V / A) + 1)
-elif (A - B) == 1:
-    print(V - B)
-else:
-    print(round(V / A) + 1)
+day = (V - B) / (A - B)
+
+print(math.ceil(day))
