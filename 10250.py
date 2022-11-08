642e1d772b768ee1c713663781971793dda86b5b
icebear2n2 <icebear2n2@gmail.com>
2022-11-08 17:41:12 +0900

    10250

--- 10250.py
+++ 10250.py
@@ -1,10 +1,10 @@
-h, w, n = map(int, input().split())
+t = int(input())
 
-if n - h > 0:
-    num = n - (h * (n // h))
-    num2 = (n // h) + 1
-else:
-    num = n - h
-    num2  = n
-
-print(num, 0, num2)
+for i in range(t):
+    h, w, n = map(int, input().split())
+    num = n//h + 1
+    floor = n % h
+    if n % h == 0:
+        num = n//h
+        floor = h
+    print(f'{floor*100+num}')
