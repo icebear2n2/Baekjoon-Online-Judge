300a990da6dba61c1b79c54b4582dd645c4be3e8
icebear2n2 <icebear2n2@gmail.com>
2022-11-21 13:01:00 +0900

    ing..

--- 2839.py
+++ 2839.py
@@ -1,4 +1,15 @@
-num1, num2 = map(int, input().split())
+# 3, 5
+# 18, 4, 6, 9, 11
+n = int(input())
 
-print(num1 + num2)
-
+if (n - 3) == 0 or (n - 5) == 0:
+    print(1)
+else:
+    while True:
+        if n % 5 == 3:
+            print((n // 5)+1)
+            break
+        else:
+            if n % 3 == 0:
+                print(n // 3)
+            break
