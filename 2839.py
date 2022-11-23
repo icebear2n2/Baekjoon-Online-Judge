b9ff190944b24cf773dfdf0df1e272e00e49b1d6
icebear2n2 <icebear2n2@gmail.com>
2022-11-23 17:31:41 +0900

    ing.........

--- 2839.py
+++ 2839.py
@@ -1,19 +1,3 @@
 # 3, 5
 # 18, 4, 6, 9, 11
-
 n = int(input())
-
-num1 = 3
-num2 = 5
-
-if n % 8 == 0:
-    print((n // 8) * 2)
-elif n % 8 == num1 or  n % 8 == num2:
-    print(((n // 8) * 2) + 1)
-else:
-    if n % num1 == 0:
-        print(n // num1)
-    elif n % num2 == 0:
-        print(n // num2)
-    else:
-        print(-1)
