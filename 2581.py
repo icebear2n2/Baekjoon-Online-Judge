183abeac9e72058ced17c03360754b4fb373435a
icebear2n2 <icebear2n2@gmail.com>
2022-11-26 12:03:24 +0900

    2581

--- 2581.py
+++ 2581.py
@@ -3,24 +3,22 @@ m = int(input())
 
 a = [False, False] + [True]*(m-1)
 
-
 primes = []
-result = []
+lst = []
 for i in range(2, m+1):
     if a[i]:
         primes.append(i)
         for j in range(2*i, m+1, i):
             a[j] = False
 
-while m > n:
-    n += 1
-    if n in primes:
-        result.append(n)
+for i in range(n, m+1):
+    if i in primes:
+        lst.append(i)
     else:
         continue
 
-if len(result) == 0:
+if len(lst) == 0:
     print(-1)
 else:
-    print(sum(result))
-    print(result[0])
+    print(sum(lst))
+    print(lst[0])
