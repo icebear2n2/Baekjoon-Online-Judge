4c1a198684483a2861223f84d404144018247fb1
icebear2n2 <icebear2n2@gmail.com>
2022-12-02 13:54:05 +0900

    ing......

--- 1929.py
+++ 1929.py
@@ -1,24 +1,21 @@
 n, m = map(int, input().split())
+def factorization(n, m):
 
-lst = []
-
-def factorization(x):
-    d = 2
-
-    while d <= x:
-        if x % d == 0:
-            if d in lst:
-                break
-            else:
-                if d < n:
+    lst = []
+    num = 0
+    for i in range(n, m+1):
+        d = 2
+        while d <= i:
+            if i % d == 0:
+                if d in lst:
                     break
-                else:
-                    lst.append(d)
-                    print(d)
-            x = x / d
-        else:
-            d = d + 1
-
-for i in range(n, m+1):
-    factorization(i) 
+                i = i / d
+                num = d
+                if num < n:
+                    break
+                print(num)
+                lst.append(num)
+            else:
+                d = d + 1
+factorization(n, m) 
 
